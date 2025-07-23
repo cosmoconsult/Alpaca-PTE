9dc0c225aac16b2befcb4343402c9ec9cba7b3f8
Ole Wunschmenn <owunschmann4046@cosmoconsult.com>
2025-07-23 17:47:54 +0200

    feat: refactor

--- .alpaca/PowerShell/Initialize-Alpaca.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module ".\.alpaca\PowerShell\module\alpaca-functions.psd1" -Scope Global -Force -DisableNameChecking
-
-$backendUrl = Get-AlpacaBackendUrl
-Write-Host "Using Backend Url '$backendUrl'"
-Add-Content -encoding UTF8 -Path $env:GITHUB_OUTPUT -Value "backendUrl=$($backendUrl)"
